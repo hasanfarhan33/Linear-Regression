8b4de1353aa47143ab7c1a1505cba2dfaf40612b
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-21 19:55:12 +0300

    Initial Commit

---
+++ linearRegression.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("C:/Users/hasan/Desktop/Programming/Jupyter Notebook Files/Udemy Courses/Python For Data Science and Machine Learning Bootcamp/Refactored_Py_DS_ML_Bootcamp-master/11-Linear-Regression/USA_Housing.csv")
+print(df.head())
